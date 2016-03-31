eac34edcbd84c7ecc903164980a97eba86142c2f
maximumayo <maxgmayo@gmail.com>
2016-03-31 13:55:28 -0700

    #1 enter key can be used to submit guess

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Guessing Game
+
+## Guess the number between 1 and 100
--- script.js
+++ script.js
@@ -3,26 +3,27 @@ $(document).ready(function () {
         game.makeGuess();
         $('#guessInput').val("");
     });
+
+    $('#guessInput').keypress(function (key) {
+        if (key.which == 13) {
+            game.makeGuess();
+            $('#guessInput').val("");
+        }
+    });
 });
 
-guessingGame = function (min, max) {
-    this.init(min, max);
+guessingGame = function () {
+    this.generateNumber();
 };
 
 guessingGame.prototype = {
     minNumber: 1,
     maxNumber: 100,
     secretNumber: null,
-    lastMessage: null,
-
-    init: function (min, max) {
-        this.minNumber = min || this.minNumber;
-        this.maxNumber = max || this.maxNumber;
-        this.generateNumber();
-    },
 
     generateNumber: function () {
         this.secretNumber = Math.floor(this.minNumber + (Math.random() * this.maxNumber));
+        console.log('secret number is: ', this.secretNumber);
     },
 
     checkGuess: function (guess) {
@@ -30,17 +31,14 @@ guessingGame.prototype = {
         if (guess < this.secretNumber) {
             $('#responseDiv').text(guess + ' is too low. try again');
             $('#gameArea').css('background-color', 'blue');
-            return false;
         }
         else if (guess > this.secretNumber) {
             $('#responseDiv').text(guess + ' is too high. try again');
             $('#gameArea').css('background-color', 'yellow');
-            return false;
         }
         else {
             $('#responseDiv').text('You Guessed It! Nice Work!');
             $('#gameArea').css('background-color', 'green');
-            return true;
         }
     },
 
