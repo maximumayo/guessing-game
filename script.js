07a3654a2175f85dd92afb116e092f559667aba0
maximumayo <maxgmayo@gmail.com>
2016-04-12 16:02:33 -0700

    basic animation and game form

--- script.js
+++ script.js
@@ -52,3 +52,56 @@ guessingGame.prototype = {
 };
 
 var game = new guessingGame();
+
+function motion(ele) {
+    var location = $('#main');
+    var height = location.height() - $(ele).height();
+    var width = location.width() - $(ele).width();
+    var rand = Math.floor(Math.random() * height);
+    var rand2 = Math.floor(Math.random() * width);
+    $(ele).animate({
+        "left": +rand + "px",
+        "top": +rand2 + "px"
+    }, 4000, function () {
+        if (!ele) {
+            return false;
+        }
+        else {
+            motion(ele);
+        }
+    });
+}
+
+//call blobs in motion
+$('document').ready(function () {
+    motion('#b1');
+    motion('#b2');
+    motion('#b3');
+    motion('#b4');
+    motion('#b5');
+    motion('#b6');
+    motion('#b7');
+    motion('#b8');
+    motion('#b9');
+    motion('#b10');
+    motion('#b11');
+    motion('#b12');
+    motion('#b13');
+    motion('#b14');
+    motion('#b15');
+    motion('#b16');
+    motion('#b17');
+    motion('#b18');
+    motion('#b19');
+    motion('#b20');
+    motion('#b21');
+    motion('#b22');
+    motion('#b23');
+    motion('#b24');
+    motion('#b25');
+    motion('#b26');
+    motion('#b27');
+    motion('#b28');
+    motion('#b29');
+    motion('#b30');
+});
